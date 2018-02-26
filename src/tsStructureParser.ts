5ad35d79cffe4fb530f7809bf4b4e1220eab19a0
NHudkov <godba11@yandex.ru>
2018-02-26 16:53:37 +0500

    tslint(#add import)

--- src/tsStructureParser.ts
+++ src/tsStructureParser.ts
@@ -27,30 +27,30 @@ var fld = tsm.Matching.field();
 
 export function parseStruct(content: string, modules: {[path: string]: Module}, mpth: string): Module {
     var mod = parse(content);
-    var module: Module = {classes: [], aliases: [], enumDeclarations: [], imports: {}, _imports:[], name: mpth};
+    var module: Module = {classes: [], aliases: [], enumDeclarations: [], imports: {}, _imports: [], name: mpth};
     modules[mpth] = module;
     var currentModule: string = null;
     tsm.Matching.visit(mod, x => {
 
-        if (x.kind ===ts.SyntaxKind.ImportDeclaration) {
-            var impDec =<ts.ImportDeclaration>x;
+        if ( x.kind === ts.SyntaxKind.ImportDeclaration ) {
+            var impDec = <ts.ImportDeclaration>x;
             var localMod = parse(x.getText());
-            var localImport = {clauses: [] , absPath:"", isNodeModule: false}
+            var localImport = { clauses: [] , absPath: "", isNodeModule: false}
             var localNamedImports: string[];
             var localAbsPath: string;
             var localNodeModule: boolean = false;
             tsm.Matching.visit(localMod, y => {
                 var _import = {}
-                if(y.kind === ts.SyntaxKind.NamedImports) {
+                if (y.kind === ts.SyntaxKind.NamedImports ) {
                     var lit = impDec.importClause.getText();
-                    localNamedImports = lit.substring(1,lit.length -1).split(',');
-                    localImport.clauses = localNamedImports.map(im => {
+                    localNamedImports = lit.substring(1, lit.length -1).split(",");
+                    localImport.clauses = localNamedImports.map( im => {
                         return im.trimLeft();
                     });;
                 }
                 if(y.kind === ts.SyntaxKind.StringLiteral) {
-                    var localPath = y.getText().substring(1,y.getText().length -1);
-                    if(localPath[0] === ".") {
+                    var localPath = y.getText().substring(1, y.getText().length -1);
+                    if ( localPath[0] === "." ) {
                         localAbsPath = fsUtil.resolve(fsUtil.dirname(mpth) + "/", localPath);
                     } else {
                         localAbsPath = localPath;
