4c194a407ad508b81720503041ed8c8b538768cc
NHudkov <godba11@yandex.ru>
2018-03-16 18:12:02 +0500

    #jsonTransformer

--- src/jsonTransformer.ts
+++ src/jsonTransformer.ts
@@ -1,4 +1,14 @@
 export class JSONTransformer {
+    public static unique(arr) {
+        let obj = {};
+    
+        for (var i = 0; i < arr.length; i++) {
+          var str = arr[i];
+          obj[str] = true;
+        }
+        return Object.keys(obj);
+    }
+
     public static toValidateView( obj: any ): string {
         let  jsonString = obj.getFullText().split("\'").join("\"");
         let matches = jsonString.match(/ [\w]+.[\w]+\(\)/);
@@ -10,15 +20,17 @@ export class JSONTransformer {
         let regExp = /: ?[a-zA-Z]\w+(\.\w+)?/g;
         let m = jsonString.match(regExp);
         if ( m ) {
+            m = m.map(item => {
+                return (<string>item).substring(1).trim();
+            })
+            m = JSONTransformer.unique(m);
             m.forEach(match => {
-                let innerReg = /[a-zA-Z]\w+(\.\w+)?/;
-                let innerMatch = innerReg.exec(match)[0];
-                let re = new RegExp(innerMatch + "[ ,}]", "");
-                if (!(innerMatch === "true" || innerMatch === "false")) {
-                    jsonString = jsonString.replace(re, `"${innerMatch}"`);
+                let re = new RegExp(match + "[ ,}]?", "g");
+                if (!(match === "true" || match === "false")) {
+                    jsonString = jsonString.replace(re, `"${match}"`);
                     let spaceReg = /" ?(\r?\n?\s+?)?"/g;
                     jsonString = jsonString.replace(spaceReg, `","`);
-                    if (jsonString[jsonString.length - 1 ] !== "}") {
+                    if (jsonString[jsonString.length - 1] !== "}") {
                         jsonString = jsonString + "}";
                     }
                 }
